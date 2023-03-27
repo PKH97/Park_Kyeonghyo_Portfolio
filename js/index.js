4bf96c041dc16e0f3780745c519eda09cc2f05bd
K.Hyo park <116334234+PKH97@users.noreply.github.com>
2023-03-27 19:31:35 +0900

    03.27 이력서 레이아웃 작성중

---
+++ js/index.js
@@ -0,0 +1,17 @@
+// const SEC  = 0.5;
+// function tab_skill_title(dom){
+//     const childs = dom.children;
+//     for(let i=0; i<childs.length; i++){
+//         childs[i].style.animationDelay = `${SEC * i}s`
+//         childs[i].classList.add('on');
+//     }
+// }
+ 
+// /* ✨ 실행 */
+// const  = document.getElementById();
+// const  = document.getElementById();
+// const  = document.getElementById();
+ 
+// clip_text(test);
+// clip_text(test2);
+// clip_text(test3);
